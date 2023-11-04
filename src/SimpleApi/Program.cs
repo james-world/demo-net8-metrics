3b044183f400acafc8400e21118f5a850f42c378
James World <james-world@users.noreply.github.com>
2023-11-04 21:56:58 +0000

    feat: RollDice endpoint added

--- src/SimpleApi/Program.cs
+++ src/SimpleApi/Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+ILogger logger = app.Logger;
+
+int RollDice()
+{
+    return Random.Shared.Next(1, 7);
+}
+
+string HandleRollDice(string? player)
+{
+    var result = RollDice();
+
+    if (string.IsNullOrEmpty(player))
+    {
+        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
+    }
+    else
+    {
+        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+    }
+
+    return result.ToString(CultureInfo.InvariantCulture);
+}
+
+app.MapGet("/rolldice/{player?}", HandleRollDice);
 
 app.Run();
